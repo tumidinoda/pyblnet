156cc8596d629ed6d3a13ddfb4863159828750de
Jonas Robert <robert.jonas@technikum-wien.at>
2019-04-28 19:26:36 +0200

    blnetMqttProxy started

---
+++ blnetMqttProxy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import time
+
+import paho.mqtt.client as mqtt
+
+from pyblnet import BLNETDirect
+
+# connect to MQTT-Broker
+mqqtClient = mqtt.Client()
+mqqtClient.connect("10.0.0.11", 1883, 60)
+mqqtClient.loop_start()
+
+# connect to BLnet
+ip = '10.0.0.170'
+blnet = BLNETDirect(ip)
+
+while True:
+    myBLnet = blnet.get_latest()
+    if myBLnet[0] == 'timout':
+        print(myBLnet[0])
+        break
+    # frame1 = myBLnet[0]
+    print(myBLnet)
+    mqqtClient.publish("tumi/heizung", str(myBLnet))
+    time.sleep(10)
