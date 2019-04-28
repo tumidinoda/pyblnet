7576454bad4a6a79b68e8517423f55365665064c
Jonas Robert <robert.jonas@technikum-wien.at>
2019-04-28 19:11:50 +0200

    mqttProxy  prototyp

--- myTest.py
+++ myTest.py
@@ -1,5 +1,33 @@
+#!/usr/bin/env python
+import time
 from pyblnet import BLNETDirect
+import paho.mqtt.client as mqtt
+import json
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("10.0.0.11", 1883, 60)
+
+client.loop_start()
 
 ip='10.0.0.170'
 blnet=BLNETDirect(ip)
-print(blnet.get_latest())
+
+
+while True:
+    myWetter=blnet.get_latest()
+    if myWetter[0]=='timout':
+        print(myWetter[0])
+        break
+    frame1=myWetter[0]
+    myWetterMQTT=str(myWetter)
+    print(myWetterMQTT)
+    client.publish("tumi/wetter", str(frame1))
+    time.sleep(300)
+
+
+
