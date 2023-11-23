4197bd30b7caab5e2489997de082a31f740896d6
JR-Technikum <jonas@technikum-wien.at>
2023-11-23 11:44:19 +0100

    Neue Messwerte in MQTT Message

--- blnetMqttProxySingle.py
+++ blnetMqttProxySingle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-import time
 
 import paho.mqtt.client as mqtt
+import json
 
 from pyblnet import BLNETDirect
 
@@ -18,5 +18,34 @@ print(myBLnet)
 if myBLnet[0] == 'timout':
     exit(1)
 mqqtClient.publish("tumi/heizung", str(myBLnet))
-exit(0)
 
+mySgrp1 = myBLnet[0]['analog']
+mySgrp2 = myBLnet[1]['analog']
+# mySgrp1=mySgrp1['analog']
+mySensorsJson = {}
+mySensorsJson["Solar_Fuehler"] = mySgrp1[1]
+mySensorsJson["Solar_Vorlauf"] = mySgrp1[2]
+mySensorsJson["Solar_Unten"] = mySgrp1[3]
+mySensorsJson["Puffer_Unten"] = mySgrp1[3]
+mySensorsJson["Puffer_Mitte"] = mySgrp1[4]
+mySensorsJson["Puffer_Oben"] = mySgrp1[5]
+mySensorsJson["Puffer_Top"] = mySgrp1[6]
+mySensorsJson["Ruecklauf"] = mySgrp1[7]
+mySensorsJson["Kessel"] = mySgrp1[8]
+mySensorsJson["Vorlauf"] = mySgrp1[9]
+mySensorsJson["Vorlauf_HK"] = mySgrp1[10]
+mySensorsJson["Vorlauf_FBH"] = mySgrp1[11]
+mySensorsJson["Raum_HK"] = mySgrp1[12]
+mySensorsJson["Raum_FBH"] = mySgrp1[13]
+mySensorsJson["Aussen"] = mySgrp1[14]
+mySensorsJson["Wasser_Mitte"] = mySgrp1[15]
+mySensorsJson["Wasser_Oben"] = mySgrp1[16]
+
+mySensorsJson["Soll_FBH"] = mySgrp2[2]
+mySensorsJson["Soll_HK"] = mySgrp2[5]
+mySensorsJson["Raum_WG"] = mySgrp2[12]
+mySensorsJson["Zirkulation_RL"] = mySgrp2[14]
+
+mqqtClient.publish("tumi/heizung/sensors", json.dumps(mySensorsJson))
+print(str(mySensorsJson))
+exit(0)
